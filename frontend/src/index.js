a3991a80c21b11045f83b1ccf88ea0da2ee76c3c
Tom <a123456@MacBook-Air-Dash.local>
2022-05-26 15:14:49 +0300

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,4 +8,5 @@ import * as ReactDOMClient from 'react-dom/client';
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
 
+
 root.render( < App / > );
