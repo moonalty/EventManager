7f33e3f07653b0a19bc94c33925c7d6486e1ec21
vasily <moonalty@gmail.com>
2022-05-26 15:20:30 +0300

    react-redux))))

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
+import Categories from "./components/Categories/Categories";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store as globalStore } from "../../redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-  <>Hello, world!</>
+    <>
+      Hello, world!
+      <Categories />
+    </>
   );
 }
 
