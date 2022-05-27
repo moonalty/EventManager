94358ca47cefe791b09ef0da2dbfb2d4f22f7fab
vasily <moonalty@gmail.com>
2022-05-27 18:10:27 +0300

    POBEDA

--- frontend/src/redux/thunk/asyncSubscribes.js
+++ frontend/src/redux/thunk/asyncSubscribes.js
@@ -4,7 +4,7 @@ export const getFetchSubs = (id) => {
   return (dispatch) => {
     fetch(`/profile`)
       .then((response) => response.json())
-      // .then((data) => console.log('DATA>>>',data))
       .then((data) => dispatch(subscribedAC(data)));
+      // .then((data) => console.log("DATA>>>", data));
   };
 };
