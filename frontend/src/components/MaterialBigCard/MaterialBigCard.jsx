331e61b7dbde0911c5d0bc5fb35ee2516a1dc623
vasily <moonalty@gmail.com>
2022-05-29 12:13:24 +0300

    поменял карточки в профиле и немного стилей добавил

---
+++ frontend/src/components/MaterialBigCard/MaterialBigCard.jsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  getFetchOneCategory,
+  getFetchSubscribe,
+} from "../../redux/thunk/asyncCategories";
+import { getFetchCheckRate, getFetchRate } from "../../redux/thunk/asyncRate";
+import RatingSystem from "../RatingSystem/RatingSystem";
+
+const OneEventCard = () => {
+  const navigator = useNavigate();
+  const { oneCategory: oneCat } = useSelector((state) => state.oneCategory);
+  const { subscribed } = useSelector((state) => state.subscribed);
+  // const { rate } = useSelector((state) => state.rate);
+  const [subState, setSubState] = React.useState(true);
+  const { el, cat } = useParams();
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(getFetchCheckRate(+el));
+    dispatch(getFetchOneCategory(el));
+    const finder = subscribed.find((ele) => ele[0].id === +el);
+    if (finder) setSubState(!subState);
+  }, [dispatch]);
+  const subscribe = () => {
+    dispatch(getFetchSubscribe(cat, el));
+    navigator(-1);
+  };
+  const setRate = (e) => {
+    e.preventDefault();
+    const rate = e.target.innerText;
+    dispatch(getFetchRate(+el, rate));
+  };
+
+  return (
+      <Card className="materialBigMainBox" sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" height="420" image={oneCat.image} alt="#" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {oneCat.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {oneCat.body}
+          </Typography>
+          <Typography>
+            Проводится с: {oneCat.date_start}
+            <br />
+            До: {oneCat.date_end}
+          </Typography>
+          <Typography>
+            Начало в: {oneCat.time_start}
+            <br />
+            Заканчивается: {oneCat.time_end}
+          </Typography>
+          <Typography>
+            Мероприятия рассчитано на -{oneCat.people_count} человек
+          </Typography>
+          <Typography>
+            Стоимость билета:
+            {oneCat.cost} рублей
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Купить билет</Button>
+          {subState && (
+            <Button onClick={subscribe} size="small">
+              Добавить в избранное
+            </Button>
+          )}
+        </CardActions>
+        <div>
+          <RatingSystem setRate={setRate} />
+        </div>
+      </Card>
+  );
+};
+
+export default OneEventCard;
