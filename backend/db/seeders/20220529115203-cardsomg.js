38e921bbdab418f941a22f41379763bc8cdc3295
vasily <moonalty@gmail.com>
2022-05-29 14:53:55 +0300

    seeders

---
+++ backend/db/seeders/20220529115203-cardsomg.js
@@ -0,0 +1,46 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Cards",
+      [
+        {
+          title: "John Doe",
+          body: "false",
+          image:
+            "https://media.istockphoto.com/photos/funny-winking-kitten-picture-id1267021092?k=20&m=1267021092&s=612x612&w=0&h=yzwxZXklHn5NwDTgKmbq2Ojtg3pga6j8K3oT7lLneAY=",
+          date_start: new Date(),
+          date_end: new Date(),
+          adress: "titty twister, 1",
+          cost: 300,
+          people_count: 30,
+          org_link: "govegas.com",
+          is_active: true,
+          user_id: 1,
+          category_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Don Joe",
+          body: "false",
+          image:
+            "https://media.istockphoto.com/photos/funny-winking-kitten-picture-id1267021092?k=20&m=1267021092&s=612x612&w=0&h=yzwxZXklHn5NwDTgKmbq2Ojtg3pga6j8K3oT7lLneAY=",
+          date_start: new Date(),
+          date_end: new Date(),
+          adress: "titty twister, 1",
+          cost: 300,
+          people_count: 30,
+          org_link: "govegas.com",
+          is_active: true,
+          user_id: 1,
+          category_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+};
