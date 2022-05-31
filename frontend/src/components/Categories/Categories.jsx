2098b0b083c9d47d662d98809d9ed3c583fb38f2
vasily <moonalty@gmail.com>
2022-05-31 10:30:22 +0300

    pagination done

--- frontend/src/components/Categories/Categories.jsx
+++ frontend/src/components/Categories/Categories.jsx
@@ -1,31 +1,40 @@
+import { Pagination } from "@mui/material";
 import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getFetchCategories } from "../../redux/thunk/asyncCategories";
-import { getFetchSubs } from "../../redux/thunk/asyncSubscribes";
 import Filter from "../Filter/Filter";
-// import TinyOneEventCard from "../TinyOneEventCard/TinyOneEventCard"; //OLD CARDS
 import TinyOneEventCard from "../MaterialTinyCards/MaterialTinyCards";
+import Paginations from "../Pagination/Pagination";
 const Categories = () => {
   const { subscribed } = useSelector((state) => state.subscribed);
   const { categories } = useSelector((state) => state.categories);
   const { cat } = useParams();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardsPerPage] = useState(4);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getFetchCategories(cat));
-    // dispatch(getFetchSubs());
   }, [cat]);
-  // console.log(cat);
+  const lastCardIndex = currentPage * cardsPerPage;
+  const firstCardIndex = lastCardIndex - cardsPerPage;
+  const currentCard = categories.slice(firstCardIndex, lastCardIndex);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <>
-    <Filter />
-    <div className="TinyMAINBOX">
-      {/* <>{list.map((el) => (el = <OneEventCard key={el.id} el={el} />))}</> */}
-      {categories.map(
-        (el) => (el = <TinyOneEventCard el={el} key={el.id} cat={cat} />)
-      )}
-    </div>
+      <Filter />
+      <div className="TinyMAINBOX">
+        {currentCard.map(
+          (el) => (el = <TinyOneEventCard el={el} key={el.id} cat={cat} />)
+        )}
+      </div>
+      <Paginations
+        cardsPerPage={cardsPerPage}
+        total={categories.length}
+        paginate={paginate}
+      />
     </>
   );
 };
