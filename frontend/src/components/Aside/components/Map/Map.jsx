8a13414789adf86287b54b605d8c0931c01b13a4
Chipokon <velemshok1695@gmail.com>
2022-05-31 19:43:41 +0300

    TryyingToGetMapByReact

--- frontend/src/components/Aside/components/Map/Map.jsx
+++ frontend/src/components/Aside/components/Map/Map.jsx
@@ -1,12 +1,79 @@
-import React from 'react';
-import styles from './map.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./map.module.css";
+import {
+  YMaps,
+  Map,
+  ZoomControl,
+  SearchControl,
+  Placemark,
+} from "react-yandex-maps";
+import { useSelector } from "react-redux";
 
-function Map() {
+// function useGeocoder(ymaps, adress){
+//   if (ymaps) {
+//     // addresses => [{geometry: [0, 0]}, {geometry: [0, 0]}]
+//     // return [{geometry: ...}, {geometry: ...}]
+//   }
+// }
+
+// function getGeometryByAddress(ymaps, adress) {
+//   return ymaps.geocode(adress).then(x => x.geoObjects.get(0).geometry);
+// }
+
+function MyMap() {
+  const [zoom, setZoom] = React.useState(9);
+  const { cards } = useSelector((state) => state.cards);
+  console.log("cardsForPlacemark!!!!!!!!!!", cards);
+  const [ymaps, setYMaps] = useState();
+  // const placemarks = useGeocoder(ymaps, adress);
+  const [placemarks, setPlacemarks] = useState([]);
+  console.log("PLACEMARKSSTATE!!!!!!!!!", placemarks);
+
+  useEffect(async () => {
+    const data = cards?.map((event) => {
+      ymaps.geocode(event.adress).then((x) => x.geoObjects.get(0).geometry.getCoordinates());
+    });
+    console.log("ssssssssss", data);
+    const cords = await Promise.all(data);
+    setPlacemarks(cords);
+    console.log("xxxxxxxxxxxxxxx", cords);
+  }, []);
+
+  const mapState = React.useMemo(
+    () => ({ center: [59.9386, 30.3141], zoom }),
+    [zoom]
+  );
+  console.log("myMAPS");
   return (
     <div className={styles.map}>
-      <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ade9e4bf79c71fd874128bbef7ccd7b5e2dd345ecfade17f831ea916b37c87e5e&ampsource=constructor"  ></iframe>
+      <YMaps
+        query={{
+          apikey: "08501024-c117-4e90-8cf0-3d8ba0ef0295",
+        }}
+      >
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <Map
+                  modules={["geocode"]}
+                  onLoad={(ymaps) => {
+                    window.ymaps = ymaps;
+                    console.log("YMAPS");
+                  }}
+                  state={mapState}
+                >
+                  <ZoomControl />
+                  {placemarks?.map((el) => (el = <Placemark />))}
+                  <SearchControl type="RouteButton" />
+                </Map>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </YMaps>
     </div>
   );
 }
 
-export default Map;
+export default MyMap;
