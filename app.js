84d3ca826e9f48abe522b6271f4faadd9779da7e
Tom <a123456@MacBook-Air-Dash.local>
2022-06-01 16:31:30 +0300

    heroku

--- app.js
+++ app.js
@@ -12,10 +12,15 @@ const mainRouter = require("./routes/main.route");
 const categoriesRouter = require("./routes/APIroutes/categories.route");
 
 config(app);
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "frontend", "build", "static")));
 
 app.use("/", mainRouter);
 app.use("/categories", categoriesRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`**** BOBM HAS BEEN PLANTED on ${PORT}`);
 });
